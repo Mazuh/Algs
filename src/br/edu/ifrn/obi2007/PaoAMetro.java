6844e9b857c9972ba9d7bea3c8f2940e60ec9834
20121044010247 <20121044010247@zn157742.ifrn.local>
2015-02-09 19:14:54 -0300

    Algoritmo de Pão a Metro incompleto. :(

---
+++ src/br/edu/ifrn/obi2007/PaoAMetro.java
@@ -0,0 +1,28 @@
+package br.edu.ifrn.obi2007;
+
+import java.util.Scanner;
+
+public class PaoAMetro {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        int n1, n2;
+        
+        System.out.print("Quantidade de pessoas: ");
+        int n = input.nextInt();
+        
+        System.out.print("Quantidade de sanduiches disponível: ");
+        int k = input.nextInt();
+        /*
+        System.out.print("Tamanhos de pães");
+        String paesEntrada = input.nextLine();
+        String paesArray[] = paesEntrada.split(" ");
+        int m[] = new int[k];
+        for (int i = 0; i < k; i++)
+            m[i] =  
+        */
+    }
+    
+}
