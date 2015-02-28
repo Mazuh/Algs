e50dc1438af9fd3d4a540beb36a479eb13f79f1d
Mazuh <marcell-mz@hotmail.com>
2015-02-28 00:26:14 -0300

    Alg do Pão a Metro finalizado com ajuda do prof. Thiago Valentim que muito explicou sobre a matemática da questão.

--- src/br/edu/ifrn/obi2007/PaoAMetro.java
+++ src/br/edu/ifrn/obi2007/PaoAMetro.java
@@ -1,5 +1,7 @@
 package br.edu.ifrn.obi2007;
 
+// http://olimpiada.ic.unicamp.br/pratique/programacao/nivel1/2007f2p1_metro
+
 import java.util.Scanner;
 
 public class PaoAMetro {
@@ -8,21 +10,42 @@ public class PaoAMetro {
         
         Scanner input = new Scanner(System.in);
         
-        int n1, n2;
-        
+        // lendo quantidade de pessoas N
         System.out.print("Quantidade de pessoas: ");
         int n = input.nextInt();
         
+        // lendo quantidade de sanduiches disponível K
         System.out.print("Quantidade de sanduiches disponível: ");
         int k = input.nextInt();
-        /*
-        System.out.print("Tamanhos de pães");
-        String paesEntrada = input.nextLine();
-        String paesArray[] = paesEntrada.split(" ");
+        
+        // criando um int vetor de K medidas M[i] de sanduiches usando uma variável auxiliar
+        System.out.println("Tamanhos de cada pedaço: ");
+        input.nextLine();
+        String mAux[] = input.nextLine().split(" ");
+        
         int m[] = new int[k];
-        for (int i = 0; i < k; i++)
-            m[i] =  
-        */
+        for (int i = 0; i < k; i++){
+            m[i] = Integer.valueOf(mAux[i]);
+        }
+        
+        // descobre o tamanho máximo igual que todos os pedaços devem ter
+        int mMaxCadaPedaco = 0;
+        for (int i = 1; i <= 10000; i++){ // o índice será a medida de cada pedaço individual 
+            // repartindo todos as medidas M em tamanhos I (índice de tentativas) 
+            int qtdPedacosDoTeste = 0;
+            for (int j = 0; j < k; j++)
+                qtdPedacosDoTeste += m[j] / i; // contando quantas partes ficaram para todos os participantes
+            
+            // a quantidade de pedaços deve ser a maior possível em que ainda assim o número de
+            // pedaços seja igual ao de pessoas.
+            if (qtdPedacosDoTeste < n){ // se o índice passar direto (começar a faltar pedaços pra pessoas)...
+                mMaxCadaPedaco = i - 1; //... é só voltar ao antecessor e acabar as buscas.
+                break;
+            }
+        }
+        
+        System.out.println("\nMedida máxima de cada pedaço: " + mMaxCadaPedaco);
+        
     }
     
 }
