91ceed69184ec6d2adc1b7ec43dd61492ca8094c
Marcell Guilherme <marcell-mz@hotmail.com>
2015-07-04 21:20:24 -0300

    Create busca.cpp

---
+++ src/obi/2012/busca.cpp
@@ -0,0 +1,15 @@
+//http://olimpiada.ic.unicamp.br/pratique/programacao/nivelj/2012f1pj_busca
+
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    
+    int t;
+    scanf("%d", &t);
+    
+    printf("%d\n", (t * 4));
+    
+    return 0;
+}
