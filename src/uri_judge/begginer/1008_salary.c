71e5f9c472e32a641554cb534ba2e6f70c861126
Marcell Guilherme Costa da Silva <Mazuh@users.noreply.github.com>
2017-03-03 14:23:15 -0300

    Create 1008_salary.c

---
+++ src/uri_judge/begginer/1008_salary.c
@@ -0,0 +1,15 @@
+/*
+https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    printf("DIFERENCA = %d\n", (a*b - c*d));
+
+    return 0;
+}
