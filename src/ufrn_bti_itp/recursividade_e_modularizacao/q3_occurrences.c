94e9db7fd6a9293366b114869da770bb54293e83
Mazuh <marcell-mz@hotmail.com>
2017-05-30 18:11:44 -0300

    Fix search for 0

--- src/ufrn_bti_itp/recursividade_e_modularizacao/q3_occurrences.c
+++ src/ufrn_bti_itp/recursividade_e_modularizacao/q3_occurrences.c
@@ -2,10 +2,10 @@
 
 int ssearch(int number, int target){
 	if (number) return ssearch(number/10, target) + (number % 10 == target);
-	else 		return !target;
+	else 		return number == target - 1;
 }
 
 int main(){
-	printf("%d\n", ssearch(762021192, 2));
+	printf("%d\n", ssearch(762021092, 0));
 	return 0;
 }
