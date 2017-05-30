900ccc2d8dc11faed7364242e50204906fac414e
Mazuh <marcell-mz@hotmail.com>
2017-05-30 17:49:49 -0300

    RULEZ

--- src/ufrn_bti_itp/recursividade_e_modularizacao/q3_occurrences.c
+++ src/ufrn_bti_itp/recursividade_e_modularizacao/q3_occurrences.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int ssearch(int number, int target){
-	if (number == 0)
-		if (target == 0) return 1;
-		else 			 return 0;
-	else
-		return ssearch(number/10, target) + (number % 10 == target);
+	if (number) return ssearch(number/10, target) + (number % 10 == target);
+	else 		return !target;
 }
 
 int main(){
