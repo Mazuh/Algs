d2888ba71235c4e4370438d55e41b3be16588a07
Mazuh <marcell-mz@hotmail.com>
2017-08-18 02:19:01 -0300

    More idiomatic choice for math lib

--- src/ufrn_lp1/lab1/q1_geometria/main.cpp
+++ src/ufrn_lp1/lab1/q1_geometria/main.cpp
@@ -1,13 +1,11 @@
-/**
- * @brief Implementação da função principal do programa
- */
- 
 #include <stdio.h>
 #include "bidimensional.h"
 #include "tridimensional.h"
 
+/**
+ * @brief Implementação da função principal do programa
+ */
 int main(){
     printf("Olá mundo!\n");
-    //Rectangle r = Rectangle(1, 2);
     return 0;
 }
--- src/ufrn_lp1/lab1/q1_geometria/tridimensional.cpp
+++ src/ufrn_lp1/lab1/q1_geometria/tridimensional.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include "tridimensional.h"
 
 /* RectangularPyramid */
